29133521155e9134f23699443839a6ae7bdc3425
Brooksteven <Brooksteven580@gmail.com>
2024-08-22 22:15:18 -0400

    inital files

---
+++ Documents/Software Engineer/GO-Programming-Language/app.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	//here we are calling a function "Print". Print is a function/command that we are calling is outputing "Hello World". You run fmt which calling print and outputting "Hello World" by running in the terminal "go run app.go"
+	fmt.Print("Hello World")
+}
---
+++ Documents/Software Engineer/GO-Programming-Language/go.mod
@@ -0,0 +1,3 @@
+module github.com/Brooksteven/GO_practice.git
+
+go 1.23.0
